182abbbfedb4aa431e3a91a9aa7c96164b9540b5
ShuaibuMaryam <shuaibumaryam113@gmail.com>
2022-10-21 09:35:56 +0200

    First commit

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import profileImg from '../Assets/profile__img.png'
+import '../Styles/header.css'
+
+function Header() {
+  return (
+    <div>
+        <div className='profile__section'>
+            <div>
+                <img src= {profileImg} alt='user display' id='profile__img'/>
+            </div>
+            <h4 id='twitter'>Maryam Shuaibu</h4>
+            <h4 id='slack'>Dabagal Maryam Shuaibu</h4>
+        </div>
+    </div>
+  )
+}
+
+export default Header
