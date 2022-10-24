e8bb8d9575e37d3fc9d083dc11657eedfafbae79
ShuaibuMaryam <shuaibumaryam113@gmail.com>
2022-10-24 05:56:55 +0200

    fixed header icon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import profileImg from '../Assets/profile__img.png'
+import headerIcon from '../Assets/header icon.png'
 import '../Styles/header.css'
 
 function Header() {
   return (
     <div>
-      <div></div>
+        <div className='header__icon'>
+          <div className='image__wrapper'>
+            <a href='google.com'><img src={headerIcon} alt='link icon'/></a>
+          </div>
+        </div>
         <div className='profile__section'>
-            <div>
+            <div className='header__image'>
                 <img src= {profileImg} alt='user display' id='profile__img'/>
-                <div className=''><i></i></div>
+                <div className='header__image__overlay'><i></i></div>
             </div>
             <h4 id='twitter'>Maryam Shuaibu</h4>
             <h4 id='slack'>Dabagal Maryam Shuaibu</h4>
